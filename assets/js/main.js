88bb754ea375fb4a5914432cb82cea0d0621f850
Jhony Souza <jhonatansouzameza100@gmail.com>
2024-01-03 04:41:24 -0300

    030120240441

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,39 @@ NAVCLOSE.onclick = toggleMenu
 /*=============== REMOVE MENU ON LINK CLICK ===============*/
 const NAVLINK = document.querySelectorAll('.nav-link').forEach(el => el.onclick = toggleMenu)
 
-
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const HEADER = document.querySelector('.header')
+const blurHeader = () => this.scrollY >= 50 ? HEADER.classList.add('blur') : HEADER.classList.remove('blur')
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const SCROLLUP = document.querySelector('.scrollup')
+const scrollUp = () => this.scrollY >= 350 ? SCROLLUP.classList.add('show') : SCROLLUP.classList.remove('show')
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const SECTIONS = document.querySelectorAll('section[id]')
+const activeSections = () => {
+  SECTIONS.forEach(el => {
+    const sectionHght = el.offsetHeight,
+    sectionTop = el.offsetTop -58,
+    sectionId = el.getAttribute('id'),
+    sectionClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+    if(scrollY > sectionTop && scrollY < sectionTop + sectionHght) {
+      sectionClass.classList.add('active-link')
+    } else {
+      sectionClass.classList.remove('active-link')
+    }
+  })
+}
+
+/*=============== COMBINE ALL SCROLL FUNCTIONS ===============*/
+const handleScroll = () => {
+  blurHeader()
+  scrollUp()
+  activeSections()
+};
+
+/*=============== ASSIGN TO WINDOW ONSCROLL ===============*/
+window.onscroll = handleScroll
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
